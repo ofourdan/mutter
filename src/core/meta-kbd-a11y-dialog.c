0b72da0ee8541b3d8acfad2a4758b75c90987d2a
Olivier Fourdan <ofourdan@redhat.com>
2017-10-31 13:58:12 +0100

    core: Add MetaKbdA11yDialog
    Add a new interface for enabling keyboard accessibility features,
    typically invoked from keyboard "toggle keys" feature, i.e. press the
    shift key 5 times in a row or keep the shift key depressed for 8 seconds
    to enable either sticky keys or slow keys.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=788564

---
+++ src/core/meta-kbd-a11y-dialog.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#include "config.h"
+
+#include "meta/meta-kbd-a11y-dialog.h"
+#include "meta/meta-enum-types.h"
+
+enum
+{
+  RESPONSE,
+  LAST_SIGNAL
+};
+
+static guint kbd_a11y_dialog_signals[LAST_SIGNAL] = { 0, };
+
+G_DEFINE_INTERFACE (MetaKbdA11yDialog, meta_kbd_a11y_dialog, G_TYPE_OBJECT)
+
+static void
+meta_kbd_a11y_dialog_default_init (MetaKbdA11yDialogInterface *iface)
+{
+  kbd_a11y_dialog_signals[RESPONSE] =
+    g_signal_new ("response",
+                  G_TYPE_FROM_INTERFACE (iface),
+                  G_SIGNAL_RUN_LAST | G_SIGNAL_ACTION,
+                  0,
+                  NULL, NULL, NULL,
+                  G_TYPE_NONE, 1, META_TYPE_KBD_A11Y_DIALOG_RESPONSE);
+}
+
+/**
+ * meta_kbd_a11y_dialog_show:
+ * @dialog: a #MetaKbdA11yDialog
+ *
+ * Shows the inhibit shortcuts dialog.
+ **/
+void
+meta_kbd_a11y_dialog_show (MetaKbdA11yDialog *dialog)
+{
+  MetaKbdA11yDialogInterface *iface;
+
+  g_return_if_fail (META_IS_KBD_A11Y_DIALOG (dialog));
+
+  iface = META_KBD_A11Y_DIALOG_GET_IFACE (dialog);
+  iface->show (dialog);
+}
+
+/**
+ * meta_kbd_a11y_dialog_hide:
+ * @dialog: a #MetaKbdA11yDialog
+ *
+ * Hides the inhibit shortcuts dialog.
+ **/
+void
+meta_kbd_a11y_dialog_hide (MetaKbdA11yDialog *dialog)
+{
+  MetaKbdA11yDialogInterface *iface;
+
+  g_return_if_fail (META_IS_KBD_A11Y_DIALOG (dialog));
+
+  iface = META_KBD_A11Y_DIALOG_GET_IFACE (dialog);
+  iface->hide (dialog);
+}
+
+/**
+ * meta_kbd_a11y_dialog_response:
+ * @dialog: a #MetaKbdA11yDialog
+ * @response: a #MetaKbdA11yDialogResponse
+ *
+ * Responds and closes the dialog. To be called by #MetaKbdA11yDialog
+ * implementations.
+ **/
+void
+meta_kbd_a11y_dialog_response (MetaKbdA11yDialog         *dialog,
+                               MetaKbdA11yDialogResponse  response)
+{
+  g_signal_emit (dialog, kbd_a11y_dialog_signals[RESPONSE], 0, response);
+  meta_kbd_a11y_dialog_hide (dialog);
+}
---
+++ src/meta/meta-kbd-a11y-dialog.h
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2017 Red Hat
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef META_KBD_A11Y_DIALOG_H
+#define META_KBD_A11Y_DIALOG_H
+
+#include <glib-object.h>
+
+#define META_TYPE_KBD_A11Y_DIALOG (meta_kbd_a11y_dialog_get_type ())
+G_DECLARE_INTERFACE (MetaKbdA11yDialog, meta_kbd_a11y_dialog,
+		     META, KBD_A11Y_DIALOG, GObject)
+
+typedef enum
+{
+  META_KBD_A11Y_DIALOG_RESPONSE_ON,
+  META_KBD_A11Y_DIALOG_RESPONSE_OFF,
+} MetaKbdA11yDialogResponse;
+
+struct _MetaKbdA11yDialogInterface
+{
+  GTypeInterface parent_iface;
+
+  void (* show) (MetaKbdA11yDialog *dialog);
+  void (* hide) (MetaKbdA11yDialog *dialog);
+};
+
+void meta_kbd_a11y_dialog_show     (MetaKbdA11yDialog        *dialog);
+void meta_kbd_a11y_dialog_hide     (MetaKbdA11yDialog        *dialog);
+void meta_kbd_a11y_dialog_response (MetaKbdA11yDialog        *dialog,
+                                    MetaKbdA11yDialogResponse response);
+
+#endif /* META_KBD_A11Y_DIALOG_H */
